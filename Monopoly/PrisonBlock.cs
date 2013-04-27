07ee3e989ed30c87fa89c1216b99c580f3666e31
¤¸¨à¡ã <yuan817@moztw.org>
2013-04-27 11:57:04 +0800

    add StationBlock, LoseBlock Class

---
+++ Monopoly/PrisonBlock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly
+{
+    class PrisonBlock : Block
+    {
+        PrisonBlock(): base()
+        {
+        }
+        public override string execute(Player p)
+        {
+            return "";
+        }
+    }
+}
